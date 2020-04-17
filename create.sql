9740a9a8492f8ea8e5018db934ad0f7321dd47e1
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-04-17 16:45:44 +0300

    Add files via upload

---
+++ create.sql
@@ -0,0 +1,24 @@
+CREATE TABLE App(
+    app_name VARCHAR2(50) NOT NULL,
+    category_name VARCHAR2(20) NOT NULL,
+    audience_name VARCHAR2(20) NOT NULL,
+    price INT NOT NULL,
+    reviews  INT NOT NULL);
+ALTER TABLE App
+    ADD CONSTRAINT app_name_pk PRIMARY KEY(app_name);    
+    
+CREATE TABLE Category(
+    category_name VARCHAR2(20) NOT NULL);
+ALTER TABLE Category
+    ADD CONSTRAINT category_name_pk PRIMARY KEY(category_name);    
+    
+CREATE TABLE Audience(
+    audience_name VARCHAR2(20) NOT NULL);
+ALTER TABLE Audience
+    ADD CONSTRAINT  audience_name_pk PRIMARY KEY( audience_name);
+    
+ALTER TABLE App
+    ADD CONSTRAINT category_name_fk FOREIGN KEY(category_name) REFERENCES Category(category_name);
+ALTER TABLE App
+    ADD CONSTRAINT audience_name_fk FOREIGN KEY(audience_name) REFERENCES Audience(audience_name);
+    
