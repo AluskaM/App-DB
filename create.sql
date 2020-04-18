9acabf3c36836b24b6ec9a4163ae51f2377b1aff
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-04-18 11:42:14 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -2,7 +2,7 @@ CREATE TABLE App(
     app_name VARCHAR2(50) NOT NULL,
     category_name VARCHAR2(20) NOT NULL,
     audience_name VARCHAR2(20) NOT NULL,
-    price NUMBER,
+    price NUMBER NOT NULL,
     reviews  INT NOT NULL);
 ALTER TABLE App
     ADD CONSTRAINT app_name_pk PRIMARY KEY(app_name);    
