a8ab78cfe1ad1e134956764c34258b60cf68d933
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-04-18 13:14:50 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,16 +1,5 @@
 
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('DoorDash - Food Delivery', 'FOOD_AND_DRINK','Everyone',0, 305034);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('My CookBook Pro (Ad Free)', 'FOOD_AND_DRINK','Everyone', 5.99, 2291);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Learn ABC Letters with Captain Cat', 'GAME_EDUCATIONAL','Everyone',2.99, 1430);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Aurora Legend', 'GAME_ROLE_PLAYING','Teen',0, 1161);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Toddler Sing and Play 2 Pro', 'GAME_MUSIC','Everyone',1.99, 246);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Kids Reading Sight Words', 'GAME_EDUCATIONAL','Everyone',2.99, 399);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Green Eggs and Ham - Dr. Seuss', 'BOOKS_AND_REFERENCE','Everyone',3.99, 745);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Meme Generator PRO', 'ENTERTAINMENT','Mature 17+',2.99, 4042);
- INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Tynker for Schools', 'GAME_EDUCATIONAL','Everyone',4.99, 33);
-INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('My Talking Pet Pro', 'ENTERTAINMENT','Everyone',9.99, 11043);
-
-
+ 
 INSERT INTO Category (category_name) VALUES ('FOOD_AND_DRINK');
 INSERT INTO Category (category_name) VALUES ('GAME_EDUCATIONAL');
 INSERT INTO Category (category_name) VALUES ('GAME_ROLE_PLAYING');
@@ -23,6 +12,17 @@ INSERT INTO Audience (audience_name) VALUES ('Everyone');
 INSERT INTO Audience (audience_name) VALUES ('Teen');
 INSERT INTO Audience (audience_name) VALUES ('Mature 17+');
 
+
+INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('DoorDash - Food Delivery', 'FOOD_AND_DRINK','Everyone',0, 305034);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('My CookBook Pro (Ad Free)', 'FOOD_AND_DRINK','Everyone', 5.99, 2291);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Learn ABC Letters with Captain Cat', 'GAME_EDUCATIONAL','Everyone',2.99, 1430);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Aurora Legend', 'GAME_ROLE_PLAYING','Teen',0, 1161);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Toddler Sing and Play 2 Pro', 'GAME_MUSIC','Everyone',1.99, 246);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Kids Reading Sight Words', 'GAME_EDUCATIONAL','Everyone',2.99, 399);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Green Eggs and Ham - Dr. Seuss', 'BOOKS_AND_REFERENCE','Everyone',3.99, 745);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Meme Generator PRO', 'ENTERTAINMENT','Mature 17+',2.99, 4042);
+ INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('Tynker for Schools', 'GAME_EDUCATIONAL','Everyone',4.99, 33);
+INSERT INTO App (app_name, category_name, audience_name, price, reviews) VALUES ('My Talking Pet Pro', 'ENTERTAINMENT','Everyone',9.99, 11043);
 
 
 
