f67159f991e74f0ccadbe395c26a6aa254c26d0f
AluskaM <31692205+AluskaM@users.noreply.github.com>
2020-04-18 13:06:39 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,12 +1,4 @@
-CREATE TABLE App(
-    app_name VARCHAR2(50) NOT NULL,
-    category_name VARCHAR2(20) NOT NULL,
-    audience_name VARCHAR2(20) NOT NULL,
-    price NUMBER NOT NULL,
-    reviews  INT NOT NULL);
-ALTER TABLE App
-    ADD CONSTRAINT app_name_pk PRIMARY KEY(app_name);    
-    
+ 
 CREATE TABLE Category(
     category_name VARCHAR2(20) NOT NULL);
 ALTER TABLE Category
@@ -16,6 +8,15 @@ CREATE TABLE Audience(
     audience_name VARCHAR2(20) NOT NULL);
 ALTER TABLE Audience
     ADD CONSTRAINT  audience_name_pk PRIMARY KEY( audience_name);
+    
+CREATE TABLE App(
+    app_name VARCHAR2(50) NOT NULL,
+    category_name VARCHAR2(20) NOT NULL,
+    audience_name VARCHAR2(20) NOT NULL,
+    price NUMBER NOT NULL,
+    reviews  INT NOT NULL);
+ALTER TABLE App
+    ADD CONSTRAINT app_name_pk PRIMARY KEY(app_name); 
     
 ALTER TABLE App
     ADD CONSTRAINT category_name_fk FOREIGN KEY(category_name) REFERENCES Category(category_name);
